790251a197e7a3030794ac71eaa2134fbead7a63
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-01-29 14:28:37 +0530

    added the route for homepage, findpeers, messager, signin and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import FindPeer from "./pages/FindPeer";
+import Messenger from "./pages/Messenger";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <p className="text-3xl font-bold underline">hello world</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/FindPeer" element={<FindPeer />} />
+        <Route path="/Messenger" element={<Messenger />} />
+      </Routes>
+    </Router>
   );
 }
 
