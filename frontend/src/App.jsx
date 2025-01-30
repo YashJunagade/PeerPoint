6b59c7b5ce85d9dbc093100bf06d8294cc8bd42c
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-01-30 17:23:13 +0530

    added profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import FindPeer from "./pages/FindPeer";
 import Messenger from "./pages/Messenger";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
         <Route path="/FindPeer" element={<FindPeer />} />
         <Route path="/Messenger" element={<Messenger />} />
       </Routes>
