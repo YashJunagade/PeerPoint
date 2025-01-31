d632947797dc387c7d5852e18a1789cdf2a1e517
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-01-31 11:28:35 +0530

    redirecting the profile page with the user id

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -71,7 +71,7 @@ function Navbar() {
                 {isProfileOpen && (
                   <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
                     <a
-                      href="/profile"
+                      href={`Profile/${user._id}`}
                       className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     >
                       <User className="w-4 h-4 mr-2" />
@@ -165,7 +165,7 @@ function Navbar() {
             <div className="pt-4 pb-3 border-t border-gray-200">
               <div className="px-2 space-y-1">
                 <a
-                  href="/Profile"
+                  href={`Profile/${user._id}`}
                   className="flex items-center px-3 py-2 rounded-md text-gray-600 hover:text-blue-600 hover:bg-gray-100"
                 >
                   <User className="w-5 h-5 mr-2" />
