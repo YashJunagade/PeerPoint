7f91c82a6af80872f2246881ca7a82271a8691d5
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-01-31 08:28:28 +0530

    signin and signup auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,71 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+//Create new User
+const signUp = async (req, res) => {
+  try {
+    const { fullname, email, password, role, college, university } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({
+      name: fullname,
+      email,
+      password,
+      role,
+      college,
+      university,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//Existing User
+const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Creadentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Creadentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "12h" }
+    );
+
+    const data = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        college: user.college,
+        university: user.university,
+      },
+    };
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { signUp, signIn };
